3141cf8c52cc5ffbf86a5f5053816850a1b1c73a
Fernandez-17 <crfernandezc@unjbg.edu.pe>
2024-07-21 22:50:48 -0500

    Se añadio la funcionalidad : Actualizar datos de producto

--- funciones.h
+++ funciones.h
@@ -19,4 +19,21 @@ void agregar_canilleras(canilleras arr[], int &n4);
 void agregar_medias(medias arr[], int &n5);
 void agregar_balones(balones arr[], int &n6);
 
+// declarar funciones para actualizar un producto
+void mostar_menu_principal( );
+
+void submenu_actualizar_camisetas(camisetas camiseta[], int& n1);
+void submenu_actualizar_shorts(shorts pantalon_corto[], int& n2);
+void submenu_actualizar_zapatillas(zapatillas zapatillas[], int& n3);
+void submenu_actualizar_canilleras(canilleras canilleras[], int& n4);
+void submenu_actualizar_medias(medias medias[], int& n5);
+void submenu_actualizar_balones(balones balones[], int& n6);
+
+void actualizar_camisetas(camisetas arr[], int &n1, int indice);
+void actualizar_shorts(shorts arr[], int &n2, int indice);
+void actualizar_zapatillas(zapatillas arr[], int &n3, int indice);
+void actualizar_canilleras(canilleras arr[], int &n4, int indice);
+void actualizar_medias(medias arr[], int &n5, int indice);
+void actualizar_balones(balones arr[], int &n6, int indice);
+
 #endif
